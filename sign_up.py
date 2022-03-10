2e15320e40be4db270a35b6ee4f608ecdec46245
dev-tyta <hardeykoryar7@gmail.com>
2022-03-11 00:18:06 +0100

    Sign Up page into a goblin game

---
+++ sign_up.py
@@ -0,0 +1,20 @@
+import game
+
+print("""Sign-Up Page. 
+    Welcome to the game input your details
+""")
+full_name = input("Input Full Name: ")
+while True:
+    user_name = input("Create aa username: ")
+    password = input("Create a Password: ")
+    if len(password) < 8:
+        print("Password has to be 8 or more numbers.")
+    else:
+        confirm_password = input("Confirm Password: ")
+        if confirm_password == password:
+            break
+        else:
+            print("Password doesn't match")
+
+print(f"Welcome {user_name}!!!!")
+game.get_input2()
