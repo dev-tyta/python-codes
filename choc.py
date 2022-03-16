38bfd4bb5ce0175039fad20389994c9b19f27591
dev-tyta <hardeykoryar7@gmail.com>
2022-03-16 10:21:48 +0100

    new code

--- choc.py
+++ choc.py
@@ -1,15 +1,47 @@
-mus_name = []
-num_mins = []
-while True:
-    print("Enter name of " + str(len(mus_name) + 1) + "musician and Minutes Listened or nothing to stop: ")
-    name = input("Musicians Name: ")
-    num = input("Minutes Listened: ")
-    if name == "":
-        break
-    mus_name.append(name)
-    num_mins.append(num)
-print(f"{mus_name} and {num_mins} ")
+# Conway's Game of Life
+import random, time, copy
 
-# keys, values = tuple(mus_name), tuple(num_mins)
-# mus_min = {keys:values}
-# print(mus_min)
+width = 60
+height = 20
+
+# Create a list of list for the cells:
+next_cells = []
+for x in range(width):
+    column = []
+    for y in range(height):
+        if random.randint(0, 1) == 0:
+            column.append("#")
+        else:
+            column.append(" ")
+    next_cells.append(column)
+
+while True:
+    print("\n\n\n\n\n\n")
+    current_cell = copy.deepcopy(next_cells)
+
+    for y in range(width):
+        for x in range(width):
+            print(current_cell[x][y], end='')
+        print()
+
+    for x in range(width):
+        for y in range(height):
+            leftcoord = (x - 1) % width
+            rightcoord = (x + 1) % width
+            abovecoord = (y - 1) % height
+            belowcoord = (y + 1) % height
+
+        numNeighbors = 0
+        if current_cell[leftcoord][abovecoord] == "#":
+            numNeighbors += 1
+        elif current_cell[x][abovecoord] == "#":
+            numNeighbors += 1
+        elif current_cell[rightcoord][abovecoord] == "#":
+            numNeighbors += 1
+        elif current_cell[leftcoord][y] == "#":
+            numNeighbors += 1
+        elif current_cell[rightcoord][y] == "#":
+            numNeighbors += 1
+        elif current_cell[leftcoord][belowcoord] == "#":
+            numNeighbors += 1
+        elif
