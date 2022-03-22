9545c47a59a7faebbf6e4addda2f1f5f47b8b155
dev-tyta <hardeykoryar7@gmail.com>
2022-03-22 11:31:58 +0100

    ammendments

--- comma.py
+++ comma.py
@@ -1,13 +1,14 @@
 spam = ['apples', 'bananas', 'tofu', 'cats']
 
 
-def listarg(spam):
+def listarg(lst):
     a = " "
-    for i in spam:
-        if i < len(spam):
-            a = a + i
-            if i == len(spam):
-                a = a + f"and {i}"
+    for i in range(len(lst)):
+        if i < len(lst):
+            a += f"{lst[i]}, "
+            if i == len(lst):
+                a += f"and {lst[i]}"
+    print(a)
 
 
-listarg()
+listarg(spam)
