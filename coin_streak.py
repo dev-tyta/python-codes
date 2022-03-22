d49915b3ffe2e782420bb5f952610bfb2f6fcf04
dev-tyta <hardeykoryar7@gmail.com>
2022-03-22 12:02:19 +0100

    CORRECTION

--- coin_streak.py
+++ coin_streak.py
@@ -5,4 +5,6 @@ for exp_num in range(10000):  # Test to be carried out 10000 times
     choice = ["H", "T"]  # The chance of having a head and a tail
     for chan in range(100):
         chance = []
-        chance = chance.append(random.choice(choice))
+        choi = random.choice(choice)  # Gets a random choice between Head and Tail
+        chance.append(choi)
+    print(chance)
