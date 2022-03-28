cb0946983de07f9a02f6344620490d1f6b346700
dev-tyta <hardeykoryar7@gmail.com>
2022-03-28 21:56:45 +0100

    nothing much

--- grid_change.py
+++ grid_change.py
@@ -1,11 +1,11 @@
-grid = [['.', '.', '.', '.', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['.', 'O', 'O', 'O', 'O', 'O'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.']]
+import pprint
 
-print(grid)
+message = 'It was a bright cold day in April, and the clocks were striking ' \
+          'thirteen.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+print(pprint.pformat(count))
