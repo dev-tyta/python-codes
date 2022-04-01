60a180bc856085fb4352eb69abb95f0f3c138dd2
dev-tyta <hardeykoryar7@gmail.com>
2022-04-01 22:03:53 +0100

    NEW

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -21,24 +21,45 @@ def score_board():
     print(f"X : {wins_X}  |  O: {wins_O}")
 
 
+print("Multiplayer or Single PLayer:(If Multiplayer input M, else input S for SinglePlayer.)")
+player_choice = input()
 print("How many Games would you love to play....")
 game_turns = int(input())
 try:
     for i in range(game_turns):
         score_board()
         turn = random.choice(["X", "O"])
-        for num in range(9):
-            print_board(ticBoard)
-            print(f"""Turn for {turn}. You are allowed to pick a space to move to.
-                They are top_L(Top Left Corner), top_M(Top Middle Space), top_R(Top Right Corner)
-                mid_L (Middle Left Space), mid_M(Middle Space), mid_R(Middle Right Space)
-                low_L(Bottom Left Corner), low_M(Bottom Middle Space), low_R(Bottom Right Corner) """)
-            move = input("Move to which space: ")
-            ticBoard[move] = turn
-            if turn == "X":
-                turn = "O"
-            else:
-                turn = "X"
+        if player_choice == "M":
+            for num in range(9):
+                print_board(ticBoard)
+                print(f"""Turn for {turn}. You are allowed to pick a space to move to.
+                     They are top_L(Top Left Corner), top_M(Top Middle Space), top_R(Top Right Corner)
+                     mid_L (Middle Left Space), mid_M(Middle Space), mid_R(Middle Right Space)
+                     low_L(Bottom Left Corner), low_M(Bottom Middle Space), low_R(Bottom Right Corner) """)
+                move = input("Move to which space: ")
+                ticBoard[move] = turn
+                if turn == "X":
+                    turn = "O"
+                else:
+                    turn = "X"
+        elif player_choice == "S":
+            for num in range(4):
+                print_board(ticBoard)
+                print(f"""Turn for {turn}. You are allowed to pick a space to move to.
+                         They are top_L(Top Left Corner), top_M(Top Middle Space), top_R(Top Right Corner)
+                         mid_L (Middle Left Space), mid_M(Middle Space), mid_R(Middle Right Space)
+                         low_L(Bottom Left Corner), low_M(Bottom Middle Space), low_R(Bottom Right Corner) """)
+                move = input("Move to which space: ")
+                rand_comp = random.choice(["X", "O"])
+                if rand_comp == turn:
+                    post = list(ticBoard.keys())
+                    comp_post = random.choice(post)
+                    ticBoard.pop(comp_post)
+                ticBoard[move] = turn
+                if turn == "X":
+                    turn = "O"
+                else:
+                    turn = "X"
 
             if ticBoard["top_L"] == ticBoard["top_M"] == ticBoard["top_R"] != " ":
                 winner = ticBoard["top_L"]
@@ -65,13 +86,13 @@ try:
                 winner = ticBoard["top_L"]
                 break
 
-        if winner == "X":
-            wins_X += 1
-        elif winner == "O":
-            wins_O += 1
-        else:
-            wins_O = 0
-            wins_X = 0
+            if winner == "X":
+                wins_X += 1
+            elif winner == "O":
+                wins_O += 1
+            else:
+                wins_O = 0
+                wins_X = 0
 
         print_board(ticBoard)
         print(f"{winner} Wins this round")
