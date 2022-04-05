24d7bf5aefc5c9c503a11bfd01ca8b19644282d1
dev-tyta <hardeykoryar7@gmail.com>
2022-04-05 14:50:04 +0100

    success

--- autocopy.py
+++ autocopy.py
@@ -5,7 +5,8 @@ TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
         'busy': """Sorry, can we do this later this week or next week?""",
         'upsell': """Would you consider making this a monthly donation"""}
 
-import sys, pyperclip
+import sys
+import pyperclip
 
 if len(sys.argv) < 2:
     print('Usage: python autocopy.py [keyphrase] - copy phrase text')
