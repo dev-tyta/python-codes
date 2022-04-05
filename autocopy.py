ae75247a4bd5c14d65d90e457dea61d28d4ebec4
dev-tyta <hardeykoryar7@gmail.com>
2022-04-05 11:58:56 +0100

    added pyperclip

--- autocopy.py
+++ autocopy.py
@@ -5,10 +5,16 @@ TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
         'busy': """Sorry, can we do this later this week or next week?""",
         'upsell': """Would you consider making this a monthly donation"""}
 
-import sys
+import sys, pyperclip
 
 if len(sys.argv) < 2:
     print('Usage: python autocopy.py [keyphrase] - copy phrase text')
     sys.exit()
 
 keyphrase = sys.argv[1]
+
+if keyphrase in TEXT:
+    pyperclip.copy(TEXT[keyphrase])
+    print(f"Text for {keyphrase} copied to clipboard.")
+else:
+    print(f"There is no text for {keyphrase}")
