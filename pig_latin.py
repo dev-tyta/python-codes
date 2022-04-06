9650d6f09971effc0b06cf31b5ff8cb13b81548c
dev-tyta <hardeykoryar7@gmail.com>
2022-04-06 14:53:23 +0100

    english to pig latin

---
+++ pig_latin.py
@@ -0,0 +1,42 @@
+print("Enter the English Message to translate into pig latin")
+message = input()
+
+vowels = ("a", "e", "i", "o", "u", "y")
+
+pigLatin = []
+for word in message.split():
+    prefix_non_letters = ""
+    while len(word) > 0 and not word[0].isalpha():
+        prefix_non_letters += word[0]
+        word = word[1:]
+    if len(word) == 0:
+        pigLatin.append(prefix_non_letters)
+        continue
+    suffix_non_letters = ""
+    while not word[-1].isalpha():
+        suffix_non_letters += word[-1]
+        word = word[:-1]
+
+    was_upper = word.isupper()
+    was_title = word.istitle()
+
+    word = word.lower()
+
+    prefix_cons = ""
+    while len(word) > 0 and not word[0] in vowels:
+        prefix_cons += word[0]
+        word = "y" + word[1:]
+
+    if [prefix_cons] != "":
+        word += prefix_cons + "ay"
+    else:
+        word += "yay"
+
+    if was_upper:
+        word = word.upper()
+    if was_title:
+        word = word.title()
+
+    pigLatin.append(prefix_non_letters + word + suffix_non_letters)
+
+print(" ".join(pigLatin))
