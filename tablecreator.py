38538239368476d852999df454ab4d458c6a35c7
dev-tyta <hardeykoryar7@gmail.com>
2022-04-15 02:30:04 +0100

    Create tablecreator.py

---
+++ tablecreator.py
@@ -0,0 +1,6 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+
+for columns in tableData:
